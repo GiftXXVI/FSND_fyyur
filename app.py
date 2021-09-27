f70815fc29537b5639534f88bdf60a254d690469
Gift Chimphonda <gift.chimphonda@outlook.com>
2021-09-27 09:59:54 +0200

    Added Null Constraints

--- app.py
+++ app.py
@@ -40,10 +40,10 @@ class Venue(db.Model):
     __tablename__ = 'venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String, nullable=False, unique=True)
     genres = db.Column(db.String(120))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
@@ -65,11 +65,11 @@ class Artist(db.Model):
     __tablename__ = 'artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String, nullable=False, unique=True)
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.String(120), nullable=False)
     website_link = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
@@ -89,11 +89,11 @@ class Show(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     artist_id = db.Column(db.Integer, db.ForeignKey(
-        'artist.id'))
+        'artist.id'), nullable=False)
     venue_id = db.Column(db.Integer, db.ForeignKey(
-        'venue.id'))
+        'venue.id'), nullable=False)
     start_time = db.Column(db.DateTime, nullable=False)
-    db.UniqueConstraint(artist_id, venue_id, start_time,name='UX_Artist_Venue')
+    db.UniqueConstraint(artist_id, venue_id, start_time, name='UX_Artist_Venue')
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
